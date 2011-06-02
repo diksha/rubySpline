9b4d09d5bc7b80994636571ff94c91b40f22da8b
Diksha <gr8bigdg@gmail.com>
2011-06-02 17:28:47 +0530

    First Commit

---
+++ public/javascripts/menuView.js
@@ -0,0 +1,44 @@
+
+		//Hides all other views and displays only the view of video
+		function hide1(){
+		  var element = document.getElementById("content");
+		  element.style.display = "none";
+  		  var item=document.getElementById('new').style.display = 'block';
+			var item2=document.getElementById('new1').style.display = 'block';
+			var item2=document.getElementById('youtube').style.display = 'none';
+ 		}
+
+		function hide2(){
+		  var item1=document.getElementById('leftmenu').style.display = 'block';
+ 		};
+ 		//Hides all other views and displays only the view of youtube option
+ 		function hide3()
+ 		{
+ 		document.getElementById("content").style.display = "none";
+ 		document.getElementById("new1").style.display = "none";
+ 		 var item=document.getElementById('new').style.display = 'none';
+ 		 		document.getElementById("youtube").style.display = 'block';
+ 		};
+ 		//Extracts the 9 letter keyword from youtube url
+ 		function Url()
+ 		{
+			var xyz=document.getElementById("url1").value;
+			var uvw="";
+			for(var i=0;i<11;i++)
+			{
+				uvw=uvw + xyz[31+i];
+
+			}
+ 			var url="http://www.youtube.com/embed/"+ uvw ;
+ 			document.getElementById("src1").src=url;
+ 			document.getElementById("url1").value=url;
+ 		};
+ 		//Hides all other views and displays only the view of home
+ 		function hide0(){
+		  var element = document.getElementById("content");
+		  element.style.display = "block";
+  		  var item=document.getElementById('new').style.display = 'none';
+			var item2=document.getElementById('new1').style.display = 'none';
+			var item2=document.getElementById('youtube').style.display = 'none';
+ 		};
+
